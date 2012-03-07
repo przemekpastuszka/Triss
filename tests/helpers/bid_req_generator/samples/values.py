b6610692c123e3913b3bd36677e9233b584ac740
Piotr Olchawa <piotrekolchawa@gmail.com>
2012-03-07 19:04:15 +0100

    fix cwd path bug in generate script

--- tests/helpers/bid_req_generator/samples/values.py
+++ tests/helpers/bid_req_generator/samples/values.py
@@ -1,29 +1,33 @@
 # Sample-content file should consis of values
 # for chosen parameter - each in new line
 import re
+import os
+# gets the prefix of the path where script residues
+# to help open() function open files with relative path
+PATH_PREFIX = os.path.split(os.path.abspath(__file__))[0]
 
 
 def parse(filename):
     """ parse sample-content files """
     res = []
-    for line in open(filename, 'r'):
+    for line in open(os.path.join(PATH_PREFIX, filename), 'r'):
         res.append(re.escape(line.strip()))
     return res
 
 sample_values = {
         'empty': [''],
         'age': [str(a) for a in range(14, 100)],
-        'os': parse('samples/operating_systems'),
-        'country': parse('samples/countries'),
-        'content': parse('samples/content'),
-        'browser': parse('samples/browser_ids'),
-        'resolution': parse('samples/resolutions'),
-        'language': parse('samples/languages'),
-        'sex': parse('samples/sex'),
-        'job': parse('samples/jobs'),
-        'name': parse('samples/names'),
-        'music': parse('samples/music'),
-        'hobby': parse('samples/hobbies'),
-        'movie_genre': parse('samples/movie_genres'),
-        'religion': parse('samples/religions')
+        'os': parse('operating_systems'),
+        'country': parse('countries'),
+        'content': parse('content'),
+        'browser': parse('browser_ids'),
+        'resolution': parse('resolutions'),
+        'language': parse('languages'),
+        'sex': parse('sex'),
+        'job': parse('jobs'),
+        'name': parse('names'),
+        'music': parse('music'),
+        'hobby': parse('hobbies'),
+        'movie_genre': parse('movie_genres'),
+        'religion': parse('religions')
 }
